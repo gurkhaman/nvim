1f55b9b3596e64c1bc717d3a6ed112fc17ac22f5
Arogya Kharel <25946015+gurkhaman@users.noreply.github.com>
2025-05-18 10:20:07 +0900

    Test todo comments

--- lua/gurkhaman/lazy/todo-comments.lua
+++ lua/gurkhaman/lazy/todo-comments.lua
@@ -1,8 +1,9 @@
 return {
+    --TODO: Test TODO
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
-        signs = false
+        signs = true
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
